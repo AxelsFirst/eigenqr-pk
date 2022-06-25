0a86f02512b3da55db2934d3f19c142ff2cfb281
SzymonF <92276802+SzymonForysiuk@users.noreply.github.com>
2022-06-25 13:43:45 +0200

    Implemented __mul__ We can now multiple two vectors

--- eigenqr/vector.py
+++ eigenqr/vector.py
@@ -17,3 +17,15 @@ class Vector(object):
         """
 
         self.vector = np.array(vector)
+
+    def __mul__(self, other):
+        """
+
+        Multiplication of vectors
+
+        Parameters:
+        -----------
+        other: an instance of a Vector class.
+
+        """
+        return Vector(np.outer(self.vector, other.vector))

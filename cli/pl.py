9cb0e45d1877c926f9b89a12b04dbe92cdfbbf00
SzymonF <92276802+SzymonForysiuk@users.noreply.github.com>
2022-06-26 14:35:13 +0200

    Implemented help and imput_matrix function in pl #18

--- cli/pl.py
+++ cli/pl.py
@@ -13,3 +13,29 @@ def help():
     print('intro: informacja')
     print('help: lista komend')
     print('end: zamkniecie aplikacji')
+
+def input_matrix():
+
+
+
+    while True:
+        print('Jaki będzie wymiar macierzy?')
+        try:
+            dimension = int(input('Wpisz tutaj'))
+
+            if dimension >0:
+                break
+            
+            else:
+                print('Wprowadź dodatnią liczbę całkowitą!')
+
+        except ValueError:
+            print('Proszę wprowadzić liczbę całkowitą!')
+
+    print('Jakie będą wartości macierzy')
+    for row_index in range(dimension):
+        while True:
+            print('Zapisz wartości dla' + i + '-tego wiersza.')
+            print('Prosze pamiętać o dzieleniu liczb spacją.')
+
+

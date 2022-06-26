c2778c505fffa1b3d913558d49d604d85a7e2202
SzymonF <92276802+SzymonForysiuk@users.noreply.github.com>
2022-06-26 14:09:10 +0200

    implemented polish version of CLI I translated the english version of CLI introduction. #17

---
+++ cli/pl.py
@@ -0,0 +1,15 @@
+def intro():
+    print('EigenQR-PK')
+    print('Przedstawiony przez')
+    print('Julia Bugaj, Dawid Kapcia, Alex Gibała, Szymon Forysiuk')
+    print(" Celem naszego projektu jest zaimplementowanie algorytmu QR do obliczania" + "wartości własnych macierzy.")
+
+def help():
+    print('Lista komend:')
+
+    print('matrix: wprowadź macierz')
+    print('deco: QR rozkład')
+    print('algo: QR algorithm')
+    print('intro: informacja')
+    print('help: lista komend')
+    print('end: zamkniecie aplikacji')

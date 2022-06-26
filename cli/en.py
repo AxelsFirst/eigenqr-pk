667b413f123202c029e05fc886028131043b8f5a
AxelsFirst <alex.gibala@protonmail.com>
2022-06-26 14:03:51 +0200

    Implemented help User can check possible commands #17

--- cli/en.py
+++ cli/en.py
@@ -8,3 +8,20 @@ def intro():
             The aim of our project is to implement a QR algorithm to calculate
              eigenvalues of a matrix.
           """)
+
+
+def help():
+    """
+
+    Function will display list of possible commands.
+
+    """
+
+    print('List of commands:')
+
+    print('matrix: input a matrix')
+    print('deco: QR decomposition')
+    print('algo: QR algorithm')
+    print('intro: info')
+    print('help: list of commands')
+    print('end: close the app')

7a5fff4831fdf2c98bc7dccf1fb608cca03784d0
AxelsFirst <alex.gibala@protonmail.com>
2022-06-26 13:54:27 +0200

    Implemented intro App will display basic informations about our project. #17

---
+++ cli/en.py
@@ -0,0 +1,10 @@
+def intro():
+    print('EigenQR-PK')
+
+    print('Brought by:')
+    print('Julia Bugaj, Dawid Kapcia, Alex Gibała, Szymon Forysiuk')
+
+    print("""
+            The aim of our project is to implement a QR algorithm to calculate
+             eigenvalues of a matrix.
+          """)
